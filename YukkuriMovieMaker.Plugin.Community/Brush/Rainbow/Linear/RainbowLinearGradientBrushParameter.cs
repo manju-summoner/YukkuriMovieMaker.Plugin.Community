0589b653ba6be3695a605875792d823026e6f455
manju-summoner <manju_summoner@manjubox.net>
2024-06-08 22:33:08 +0900

    並び順を変更

--- YukkuriMovieMaker.Plugin.Community/Brush/Rainbow/Linear/RainbowLinearGradientBrushParameter.cs
+++ YukkuriMovieMaker.Plugin.Community/Brush/Rainbow/Linear/RainbowLinearGradientBrushParameter.cs
@@ -15,10 +15,18 @@ namespace YukkuriMovieMaker.Plugin.Community.Brush.Rainbow.Linear
         [AnimationSlider("F1", "px", 0, 500)]
         public Animation Width { get; } = new Animation(100);
 
-
         [Display(Name = nameof(Texts.Offset), ResourceType = typeof(Texts))]
         [AnimationSlider("F1", "px", -500, 500)]
         public Animation Offset { get; } = new Animation(0);
+
+        [Display(Name = nameof(Texts.Angle), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation Angle { get; } = new Animation(0, -36000, 36000);
+
+        [Display(Name = nameof(Texts.Extend), ResourceType = typeof(Texts))]
+        [EnumComboBox]
+        public ExtendMode ExtendMode { get => extendMode; set => Set(ref extendMode, value); }
+        ExtendMode extendMode = ExtendMode.Wrap;
 
         [Display(Name = nameof(Texts.ColorSpace), ResourceType = typeof(Texts))]
         [EnumComboBox]
@@ -33,14 +41,6 @@ namespace YukkuriMovieMaker.Plugin.Community.Brush.Rainbow.Linear
         [AnimationSlider("F1", "%", 0, 100)]
         public Animation Brightness { get; } = new Animation(100, 0, 100);
 
-        [Display(Name = nameof(Texts.Angle), ResourceType = typeof(Texts))]
-        [AnimationSlider("F1", "°", -360, 360)]
-        public Animation Angle { get; } = new Animation(0, -36000, 36000);
-
-        [Display(Name = nameof(Texts.Extend), ResourceType = typeof(Texts))]
-        [EnumComboBox]
-        public ExtendMode ExtendMode { get => extendMode; set => Set(ref extendMode, value); }
-        ExtendMode extendMode = ExtendMode.Wrap;
 
 
         public override IBrushSource CreateBrush(IGraphicsDevicesAndContext devices)
