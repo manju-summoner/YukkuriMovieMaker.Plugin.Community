0e3da6b186f114b447a907f7a79ca8df5a64944f
manju-summoner <manju_summoner@manjubox.net>
2024-06-09 00:46:47 +0900

    追加: 音量（dB）エフェクト

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Audio/DecibelVolume/DecibelVolumeEffect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Audio.Effects;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Audio.DecibelVolume
+{
+    [AudioEffect(nameof(Texts.DecibelVolumeEffect), [AudioEffectCategories.Basic], ["gain","volume","ゲイン", "ボリューム"], IsAviUtlSupported = false, ResourceType = typeof(Texts))]
+    internal class DecibelVolumeEffect : AudioEffectBase
+    {
+        public override string Label => Texts.DecibelVolumeEffect;
+
+        [Display(GroupName = nameof(Texts.DecibelVolumeEffect), Name = nameof(Texts.DecibelName), Description = nameof(Texts.DecibelDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "dB", -10, 10)]
+        public Animation Decibel { get; } = new Animation(0, -60, 60);
+
+        public override IAudioEffectProcessor CreateAudioEffect(TimeSpan duration)
+        {
+            return new DecibelVolumeEffectProcessor(this);
+        }
+
+        public override IEnumerable<string> CreateExoAudioFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            var fps = exoOutputDescription.VideoInfo.FPS;
+            yield return $"_name=アニメーション効果\r\n" +
+                $"_disable={(IsEnabled ? 0 : 1)}\r\n" +
+                $"track0={Decibel.ToExoString(keyFrameIndex, "F1", fps)}\r\n" +
+                $"name=音量dB@YMM4-未実装\r\n" +
+                $"param=\r\n";
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Decibel];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Audio/DecibelVolume/DecibelVolumeEffectProcessor.cs
@@ -0,0 +1,34 @@
+﻿using YukkuriMovieMaker.Player.Audio.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Audio.DecibelVolume
+{
+    internal class DecibelVolumeEffectProcessor(DecibelVolumeEffect item) : AudioEffectProcessorBase
+    {
+        public override int Hz => Input?.Hz ?? throw new InvalidOperationException();
+
+        public override long Duration => Input?.Duration ?? throw new InvalidOperationException();
+
+        long currentPosition = 0;
+
+        protected override int read(float[] destBuffer, int offset, int count)
+        {
+            if(Input is null)
+                return 0;
+            var read = Input.Read(destBuffer, offset, count);
+            for (int i = 0; i < read; i++)
+            {
+                var dB = item.Decibel.GetValue((currentPosition + i) % 2, Duration % 2, Hz);
+                var gain = dB <= -60 ? 0 : (float)Math.Pow(10, dB / 20);
+                destBuffer[offset + i] *= gain;
+            }
+            currentPosition += read;
+            return read;
+        }
+
+        protected override void seek(long position)
+        {
+            currentPosition = position;
+            Input?.Seek(position);
+        }
+    }
+}
