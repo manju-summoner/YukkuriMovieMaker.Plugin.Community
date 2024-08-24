ad74f5d389578c87cb133afcebf17fab46e379af
べにかずら <155831126+benikazura@users.noreply.github.com>
2024-08-24 14:56:14 +0900

    矢絣柄2を追加

---
+++ YukkuriMovieMaker.Plugin.Community/Brush/Pattern/Arrow2/ArrowPatternBrush2Plugin.cs
@@ -0,0 +1,14 @@
+﻿using YukkuriMovieMaker.Plugin.Brush;
+
+namespace YukkuriMovieMaker.Plugin.Community.Brush.Pattern.Arrow2
+{
+    internal class ArrowPatternBrush2Plugin : IBrushPlugin
+    {
+        public string Name => Texts.ArrowPattern2;
+
+        public IBrushParameter CreateBrushParameter()
+        {
+            return new ArrowPatternBrush2Parameter();
+        }
+    }
+}
