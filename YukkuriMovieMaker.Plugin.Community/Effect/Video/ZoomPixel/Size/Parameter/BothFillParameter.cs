f503318fdfdb3217801135549322f482cffe540b
manju summoner <manju_summoner@manjubox.net>
2024-10-02 20:27:26 +0900

    文字列補間式に変更

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/ZoomPixel/Size/Parameter/BothFillParameter.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/ZoomPixel/Size/Parameter/BothFillParameter.cs
@@ -18,7 +18,7 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.ZoomPixel.Size.Paramet
         [AnimationSlider("F1", "px", 0, 500)]
         public Animation Height { get; } = new Animation(100, 0, 5000);
 
-        public override string Label => String.Format(Texts.Width + "{0:0.0}px, " + Texts.Height + "{1:0.0}px", Width.Values[0].Value, Height.Values[0].Value);
+        public override string Label => $"{Texts.Width}{Width.Values[0].Value:F1}px, {Texts.Height}{Height.Values[0].Value:F1}px";
 
         public BothFillParameter()
         {
