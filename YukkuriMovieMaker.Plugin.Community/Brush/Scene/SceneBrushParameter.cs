d33779c1c02483271692e34e2531fd9c521711a5
manju summoner <manju_summoner@manjubox.net>
2024-12-17 18:02:07 +0900

    模様「シーン」を追加

---
+++ YukkuriMovieMaker.Plugin.Community/Brush/Scene/SceneBrushParameter.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Plugin.Brush;
+using YukkuriMovieMaker.Plugin.Community.Commons;
+
+namespace YukkuriMovieMaker.Plugin.Community.Brush.Scene
+{
+    internal class SceneBrushParameter : BrushParameterBase
+    {
+        [Display(Name = nameof(Texts.Scene), ResourceType = typeof(Texts))]
+        [SceneComboBox]
+        public Guid SceneId { get => sceneId; set => Set(ref sceneId, value); }
+        Guid sceneId;
+
+
+        [Display(Name = nameof(Texts.X), Description = nameof(Texts.X), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", -500, 500)]
+        public Animation X { get; } = new Animation(0);
+
+        [Display(Name = nameof(Texts.Y), Description = nameof(Texts.Y), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", -500, 500)]
+        public Animation Y { get; } = new Animation(0);
+
+        [Display(Name = nameof(Texts.Zoom), Description = nameof(Texts.Zoom), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "%", 0, 400)]
+        public Animation Zoom { get; } = new Animation(100, 0, 5000);
+
+        [Display(Name = nameof(Texts.Angle), Description = nameof(Texts.Angle), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation Angle { get; } = new Animation(0, -36000, 36000);
+
+        [Display(Name = nameof(Texts.PlaybackRate), Description = nameof(Texts.PlaybackRate), ResourceType = typeof(Texts))]
+        [TextBoxSlider("F1", "%", -400, 400)]
+        [DefaultValue(100)]
+        [Range(int.MinValue, int.MaxValue)]
+        public double PlaybackRate { get => playbackRate; set => Set(ref playbackRate, value); }
+        double playbackRate = 100;
+
+        [Display(Name = nameof(Texts.ContentOffset), Description = nameof(Texts.ContentOffset), ResourceType = typeof(Texts))]
+        [TimeSpanTextEditor]
+        public TimeSpan ContentOffset { get => contentOffset; set=>Set(ref contentOffset,value); }
+        TimeSpan contentOffset = TimeSpan.Zero;
+
+        [Display(Name = nameof(Texts.IsInverted), Description = nameof(Texts.IsInverted), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsInverted { get => isInverted; set => Set(ref isInverted, value); }
+        bool isInverted = false;
+
+        [Display(Name = nameof(Texts.ExtendModeX), Description = nameof(Texts.ExtendModeX), ResourceType = typeof(Texts))]
+        [EnumComboBox]
+        public ExtendMode ExtendModeX { get => extendMode; set => Set(ref extendMode, value); }
+        ExtendMode extendMode = ExtendMode.Wrap;
+
+        [Display(Name = nameof(Texts.ExtendModeY), Description = nameof(Texts.ExtendModeY), ResourceType = typeof(Texts))]
+        [EnumComboBox]
+        public ExtendMode ExtendModeY { get => extendModeY; set => Set(ref extendModeY, value); }
+        ExtendMode extendModeY = ExtendMode.Wrap;
+
+        [Display(Name = nameof(Texts.RemoveBoundary), Description = nameof(Texts.RemoveBoundary), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsRemoveBoundaryEnabled { get => isRemoveBoundaryEnabled; set => Set(ref isRemoveBoundaryEnabled, value); }
+        bool isRemoveBoundaryEnabled = true;
+
+        [Display(Name = nameof(Texts.FixSize), Description = nameof(Texts.FixSize), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsFixSizeEnabled { get => isFixSizeEnabled; set => Set(ref isFixSizeEnabled, value); }
+        bool isFixSizeEnabled = false;
+
+
+        public override IBrushSource CreateBrush(IGraphicsDevicesAndContext devices)
+        {
+            return new SceneBrushSource(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [X, Y, Zoom, Angle];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Brush/Scene/SceneBrushSource.cs
@@ -0,0 +1,174 @@
+﻿using System.Drawing;
+using System.Numerics;
+using System.Windows;
+using Vortice;
+using Vortice.Direct2D1;
+using Windows.Win32.UI.Input.Ime;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Brush;
+using YukkuriMovieMaker.Plugin.Community.Commons;
+
+namespace YukkuriMovieMaker.Plugin.Community.Brush.Scene
+{
+    internal class SceneBrushSource(IGraphicsDevicesAndContext devices, SceneBrushParameter parameter) : IBrushSource
+    {
+        readonly IGraphicsDevicesAndContext devices = devices;
+        readonly SceneBrushParameter parameter = parameter;
+        readonly DisposeCollector disposer = new();
+
+        public ID2D1Brush Brush => brush ?? throw new NullReferenceException();
+
+        Guid sceneId;
+        RawRectF bounds;
+
+        ITimelineSource? source;
+        ID2D1Bitmap? sourceBitmap;
+        ID2D1BitmapBrush? brush;
+
+        public bool Update(TimelineItemSourceDescription desc)
+        {
+            var frame = desc.ItemPosition.Frame;
+            var length = desc.ItemDuration.Frame;
+            var fps = desc.FPS;
+            var dc = devices.DeviceContext;
+
+            var sceneId = parameter.SceneId;
+            var x = parameter.X.GetValue(frame, length, fps);
+            var y = parameter.Y.GetValue(frame, length, fps);
+            var zoom = parameter.Zoom.GetValue(frame, length, fps);
+            var angle = parameter.Angle.GetValue(frame, length, fps);
+            var playbackRate = parameter.PlaybackRate;
+            var contentOffset = parameter.ContentOffset;
+            var extendModeX = parameter.ExtendModeX.ToD2DExtendMode();
+            var extendModeY = parameter.ExtendModeY.ToD2DExtendMode();
+            var isInverted = parameter.IsInverted;
+            var isRemoveBoudalyEnabled = parameter.IsRemoveBoundaryEnabled;
+            var isFixSizeEnabled = parameter.IsFixSizeEnabled;
+
+            //シーンが変わったらソースを作り直す
+            var targetScene = desc.Scenes.FirstOrDefault(x => x.ID == sceneId);
+            if (this.sceneId != sceneId)
+            {
+                if(source != null)
+                    disposer.RemoveAndDispose(ref source);
+                if (targetScene != null && targetScene.TryCreateVideoSource(devices, out source))
+                    disposer.Collect(source);
+            }
+
+            //シーンを更新する
+            var time = targetScene != null ? TimeSpan.FromTicks((contentOffset.Ticks + desc.ItemPosition.Time.Ticks) % targetScene.Duration.Time.Ticks) : TimeSpan.Zero;
+            source?.Update(time, desc.Usage);
+
+            //描画先の画像サイズを取得
+            int width, height;
+            Vector2 offset;
+            if (source?.Output != null && targetScene != null)
+            {
+                bounds = dc.GetImageLocalBounds(source.Output);
+                if (isFixSizeEnabled)
+                {
+                    width = targetScene.Width;
+                    height = targetScene.Height;
+                    offset = new Vector2(width/2f, height/2f);
+                }
+                else
+                {
+                    width = (int)(bounds.Right - bounds.Left);
+                    height = (int)(bounds.Bottom - bounds.Top);
+                    offset = new Vector2(-bounds.Left, -bounds.Top);
+                    if(isRemoveBoudalyEnabled)
+                    {
+                        if (3 < width)
+                        {
+                            width -= 2;
+                            offset = offset + new Vector2(-1, 0);
+                        }
+                        if (3 < height)
+                        {
+                            height -= 2;
+                            offset = offset + new Vector2(0, -1);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                bounds = new RawRectF(0, 0, 0, 0);
+                width = 10;
+                height = 10;
+                offset = new Vector2(5, 5);
+            }
+
+            //描画先の画像を作り直す
+            if (sourceBitmap is null || sourceBitmap.PixelSize.Width != width || sourceBitmap.PixelSize.Height != height)
+            {
+                if(sourceBitmap != null)
+                    disposer.RemoveAndDispose(ref sourceBitmap);
+                sourceBitmap = dc.CreateEmptyBitmap(width, height);
+                disposer.Collect(sourceBitmap);
+            }
+
+            //描画先の画像に描画
+            dc.Target = sourceBitmap;
+            dc.BeginDraw();
+            dc.Clear(null);
+            if(source != null)
+                dc.DrawImage(source.Output, offset);
+            dc.EndDraw();
+            dc.Target = null;
+
+            //ブラシを作り直す
+            if (brush != null)
+                disposer.RemoveAndDispose(ref brush);
+            brush = dc.CreateBitmapBrush(
+                sourceBitmap,
+                new BitmapBrushProperties1(extendModeX, extendModeY, InterpolationMode.MultiSampleLinear),
+                new BrushProperties(
+                    1f,
+                    Matrix3x2.CreateTranslation(-sourceBitmap.PixelSize.Width / 2, -sourceBitmap.PixelSize.Height / 2)
+                    * Matrix3x2.CreateRotation((float)angle / 180 * MathF.PI)
+                                * Matrix3x2.CreateScale((float)zoom / 100)
+                                * Matrix3x2.CreateScale(isInverted ? -1 : 1, 1)
+                                * Matrix3x2.CreateTranslation((float)x, (float)y)));
+            disposer.Collect(brush);
+
+            this.sceneId = sceneId;
+            return true;
+
+        }
+
+        #region IDisposable Support
+        private bool disposedValue;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // マネージド状態を破棄します (マネージド オブジェクト)
+                    disposer.Dispose();
+                }
+
+                // アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~SceneBrushSource()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
