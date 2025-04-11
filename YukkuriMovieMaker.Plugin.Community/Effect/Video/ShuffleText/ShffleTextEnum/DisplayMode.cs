c162c0e7bd07f8ac0a6489d7fdea55f0900f3aa5
Dolphin-kun <dolphin.discord.js@gmail.com>
2025-04-11 20:15:26 +0900

    シャッフルしながら登場退場エフェクトを追加

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/ShuffleText/ShffleTextEnum/DisplayMode.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.ShuffleText.ShffleTextEnum
+{
+    public enum DisplayMode
+    {
+        [Display(Name = nameof(Texts.ShuffleTextEffectEnum_DisplayMode_Simultaneous), Description = nameof(Texts.ShuffleTextEffectEnum_DisplayMode_Discription_Simultaneous), ResourceType = typeof(Texts))]
+        Nomal = 1,
+
+        [Display(Name = nameof(Texts.ShuffleTextEffectEnum_DisplayMode_Order), Description = nameof(Texts.ShuffleTextEffectEnum_DisplayMode_Discription_Order), ResourceType = typeof(Texts))]
+        Order = 2,
+    }
+}
