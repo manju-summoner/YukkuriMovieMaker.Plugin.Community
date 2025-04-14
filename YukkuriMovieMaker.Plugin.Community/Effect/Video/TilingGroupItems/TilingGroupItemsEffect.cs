0300f06c145b35cc4efa0fe985d8140ad5043d78
manju summoner <manju_summoner@manjubox.net>
2025-04-14 23:58:36 +0900

    グループ制御中のアイテムで画面分割

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/TilingGroupItems/TilingGroupItemsEffect.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.TilingGroupItems
+{
+    [VideoEffect(nameof(Texts.TilingGroupItemsEffectName), [VideoEffectCategories.Layout], ["整列", "Tiling items in group control"], IsEffectItemSupported = false, IsAviUtlSupported = false, ResourceType = typeof(Texts))]
+
+    internal class TilingGroupItemsEffect : VideoEffectBase
+    {
+        public override string Label => Texts.TilingGroupItemsEffectName;
+
+        [Display(GroupName = nameof(Texts.TilingGroupItemsEffectName), Name = nameof(Texts.Columns), Description = nameof(Texts.Columns), ResourceType = typeof(Texts))]
+        [AnimationSlider("F0", "", 1, 5)]
+        public Animation Columns { get; } = new Animation(1, 1, 100000);
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+
+            var fps = exoOutputDescription.VideoInfo.FPS;
+            yield return
+                $"_name=アニメーション効果\r\n" +
+                $"_disable={(IsEnabled ? 0 : 1)}\r\n" +
+                $"track0={Columns.ToExoString(keyFrameIndex, "F2", fps)}\r\n" +
+                $"name=グループ制御中のアイテムで画面分割@YMM4\r\n" +
+                $"param=" +
+                    $"\r\n";
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new TilingGroupItemsEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Columns];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/TilingGroupItems/TilingGroupItemsEffectProcessor.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+using System.Windows;
+using Vortice.Direct2D1;
+using Vortice.Direct2D1.Effects;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Player.Video.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.TilingGroupItems
+{
+    internal class TilingGroupItemsEffectProcessor(IGraphicsDevicesAndContext devices, TilingGroupItemsEffect item) : VideoEffectProcessorBase(devices)
+    {
+        readonly IGraphicsDevicesAndContext devices = devices;
+        readonly TilingGroupItemsEffect item = item;
+
+        public override DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var groupIndex = effectDescription.GroupIndex;
+            var groupCount = effectDescription.GroupCount;
+
+            var columns = (int)item.Columns.GetValue(frame, length, fps);
+            var rows = (int)Math.Ceiling((double)groupCount / columns);
+            var columnIndex = groupIndex % columns;
+            var rowIndex = groupIndex / columns;
+            if (rowIndex == rows - 1)
+                columns -= columns * rows - groupCount;
+
+            var screen = effectDescription.ScreenSize;
+            var cwllWidth = (double)screen.Width / columns;
+            var cwllHeight = (double)screen.Height / rows;
+            var dx = (float)(cwllWidth * columnIndex + cwllWidth / 2 - (double)screen.Width / 2);
+            var dy = (float)(cwllHeight * rowIndex + cwllHeight / 2 - (double)screen.Height / 2);
+
+            var bounds = devices.DeviceContext.GetImageLocalBounds(input);
+            double scale = 1;
+            if (bounds.Left != 0)
+                scale = Math.Min(scale, cwllWidth / 2 / Math.Abs(bounds.Left));
+            if (bounds.Right != 0)
+                scale = Math.Min(scale, cwllWidth / 2 / Math.Abs(bounds.Right));
+            if (bounds.Top != 0)
+                scale = Math.Min(scale, cwllHeight / 2 / Math.Abs(bounds.Top));
+            if (bounds.Bottom != 0)
+                scale = Math.Min(scale, cwllHeight / 2 / Math.Abs(bounds.Bottom));
+
+            var desc = effectDescription.DrawDescription;
+            return desc with
+            {
+                Draw = desc.Draw + new Vector3(dx, dy, 0),
+                Zoom = desc.Zoom * new Vector2((float)scale, (float)scale),
+            };
+        }
+
+        protected override ID2D1Image? CreateEffect(IGraphicsDevicesAndContext devices)
+        {
+            return null;
+        }
+
+        protected override void setInput(ID2D1Image? input)
+        {
+
+        }
+
+        protected override void ClearEffectChain()
+        {
+
+        }
+    }
+}
