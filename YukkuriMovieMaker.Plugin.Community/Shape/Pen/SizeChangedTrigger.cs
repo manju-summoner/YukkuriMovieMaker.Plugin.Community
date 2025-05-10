3b6b5e59569ea2cc8f1a94926d1e763861ce7a7a
manju summoner <manju_summoner@manjubox.net>
2025-05-10 22:28:15 +0900

    図形「手書きペン」のペンツールで、カーソルサイズが実際のペンの大きさと異なっている問題を修正

---
+++ YukkuriMovieMaker.Plugin.Community/Shape/Pen/SizeChangedTrigger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
+{
+    internal class SizeChangedTrigger : TriggerBase<FrameworkElement>
+    {
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SizeChanged += OnSizeChanged;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.SizeChanged -= OnSizeChanged;
+        }
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            InvokeActions(e);
+        }
+    }
+}
