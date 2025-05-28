a8f7735c10da0c7caee39a65306aee1eaec34848
manju summoner <manju_summoner@manjubox.net>
2025-05-28 20:08:49 +0900

    モデル一覧の誤表記修正？モデル名変更？に対応 https://ai.google.dev/gemini-api/docs/speech-generation

--- YukkuriMovieMaker.Plugin.Community/Voice/GeminiTTS/GeminiTTSVoices.cs
+++ YukkuriMovieMaker.Plugin.Community/Voice/GeminiTTS/GeminiTTSVoices.cs
@@ -18,7 +18,7 @@ namespace YukkuriMovieMaker.Plugin.Community.Voice.GeminiTTS
                 "Leda",
                 "Orus",
                 "Aoede",
-                "Callirhoe",
+                "Callirrhoe",
                 "Autonoe",
                 "Enceladus",
                 "Iapetus",
@@ -39,7 +39,7 @@ namespace YukkuriMovieMaker.Plugin.Community.Voice.GeminiTTS
                 "Vindemiatrix",
                 "Sadachbia",
                 "Sadaltager",
-                "Sulafar",
+                "Sulafat",
             ];
     }
 }
