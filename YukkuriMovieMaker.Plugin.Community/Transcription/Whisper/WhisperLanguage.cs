86ccd7e225b1e47bbf3c6ca2f87980b2f399410b
manju summoner <manju_summoner@manjubox.net>
2025-06-01 17:59:05 +0900

    一部の環境でYMM4が起動しない問題を修正

--- YukkuriMovieMaker.Plugin.Community/Transcription/Whisper/WhisperLanguage.cs
+++ YukkuriMovieMaker.Plugin.Community/Transcription/Whisper/WhisperLanguage.cs
@@ -1,11 +1,12 @@
-﻿using Whisper.net;
+﻿using System.Globalization;
+using Whisper.net;
 
 namespace YukkuriMovieMaker.Plugin.Community.Transcription.Whisper
 {
     internal readonly record struct WhisperLanguage
     {
         public static WhisperLanguage Auto { get; } = new("auto");
-        public static IReadOnlyList<WhisperLanguage> SupportedLanguages { get; } = [Auto, .. WhisperFactory.GetSupportedLanguages().Select(x => new WhisperLanguage(x))];
+        public static IReadOnlyList<WhisperLanguage> SupportedLanguages { get; } = [Auto, .. GetSupportedLanguagesSafe().Select(x => new WhisperLanguage(x))];
 
         public string Code { get; }
         public bool IsAuto => this == Auto;
@@ -17,10 +18,29 @@ namespace YukkuriMovieMaker.Plugin.Community.Transcription.Whisper
 
         public bool EqualsCode(string code) => Code.Equals(code, StringComparison.OrdinalIgnoreCase);
 
+        static IEnumerable<string> GetSupportedLanguagesSafe() 
+        {            
+            try
+            {
+                return WhisperFactory.GetSupportedLanguages();
+            }
+            catch
+            {
+                return [];
+            }
+        }
+
         public static WhisperLanguage GetSystemLanguageOrAuto()
         {
-            var systemLanguage = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
-            return SupportedLanguages.DefaultIfEmpty(Auto).First(x => x.EqualsCode(systemLanguage));
+            try
+            {
+                var systemLanguage = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+                return SupportedLanguages.DefaultIfEmpty(Auto).First(x => x.EqualsCode(systemLanguage));
+            }
+            catch
+            {
+                return Auto;
+            }
         }
     }
 }
