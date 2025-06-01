4a85aa14e96588a05b7659ffb2fbce1107b501b8
manju summoner <manju_summoner@manjubox.net>
2025-06-02 01:10:48 +0900

    未使用コードを削除

--- YukkuriMovieMaker.Plugin.Community/Transcription/Whisper/WhisperTranscriptionSettingsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using YukkuriMovieMaker.Commons;
-
-namespace YukkuriMovieMaker.Plugin.Community.Transcription.Whisper
-{
-    internal class WhisperTranscriptionSettingsViewModel : Bindable
-    {
-        public IReadOnlyList<WhisperModel> Models { get; } = WhisperModels.GetDefaultAndUserModels();
-    }
-}
