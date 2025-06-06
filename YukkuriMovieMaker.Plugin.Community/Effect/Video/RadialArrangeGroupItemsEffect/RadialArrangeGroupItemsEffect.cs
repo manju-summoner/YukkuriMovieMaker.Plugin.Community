7cff9d308cafdb2c604a9849a3152c92fb14cf16
manju summoner <manju_summoner@manjubox.net>
2025-06-06 17:33:45 +0900

    「円弧」「方向」「中心寄せ」を追加

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/RadialArrangeGroupItemsEffect/RadialArrangeGroupItemsEffect.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/RadialArrangeGroupItemsEffect/RadialArrangeGroupItemsEffect.cs
@@ -22,10 +22,23 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RadialArrangeGroupItem
         [AnimationSlider("F1", "px", -100, 100)]
         public Animation Radius { get; } = new Animation(0, YMM4Constants.VerySmallValue, YMM4Constants.VeryLargeValue);
 
+        [Display(GroupName = nameof(Texts.RadialArrangeGroupItemsEffectName), Name = nameof(Texts.Arc), Description = nameof(Texts.Arc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", 0, 360)]
+        public Animation Arc { get; } = new Animation(360, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.RadialArrangeGroupItemsEffectName), Name = nameof(Texts.Direction), Description = nameof(Texts.Direction), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation Direction { get; } = new Animation(0, YMM4Constants.VerySmallValue, YMM4Constants.VeryLargeValue);
+
         [Display(GroupName = nameof(Texts.RadialArrangeGroupItemsEffectName), Name = nameof(Texts.SyncAngle), Description = nameof(Texts.SyncAngle), ResourceType = typeof(Texts))]
         [ToggleSlider]
         public bool IsRotationSynchronized { get => isRotationSynchronized; set => Set(ref isRotationSynchronized, value); }
         bool isRotationSynchronized = false;
+
+        [Display(GroupName = nameof(Texts.RadialArrangeGroupItemsEffectName), Name = nameof(Texts.Centering), Description = nameof(Texts.Centering), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsCentering { get=> isCentering; set => Set(ref isCentering, value); }
+        bool isCentering = false;
 
         public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
         {
@@ -35,9 +48,12 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RadialArrangeGroupItem
                 $"_name=アニメーション効果\r\n" +
                 $"_disable={(IsEnabled ? 0 : 1)}\r\n" +
                 $"track0={Radius.ToExoString(keyFrameIndex, "F2", fps)}\r\n" +
+                $"track1={Arc.ToExoString(keyFrameIndex, "F2", fps)}\r\n" +
+                $"track2={Direction.ToExoString(keyFrameIndex, "F2", fps)}\r\n" +
                 $"name=グループ制御中のアイテムを円周上に均等配置@YMM4\r\n" +
                 $"param=" +
                     $"local isRotationSynchronized={(IsRotationSynchronized ? 1 : 0)};" +
+                    $"local isCentering={(IsCentering ? 1 : 0)}" +
                     $"\r\n";
         }
 
@@ -46,6 +62,6 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RadialArrangeGroupItem
             return new RadialArrangeGroupItemsEffectProcessor(devices, this);
         }
 
-        protected override IEnumerable<IAnimatable> GetAnimatables() => [Radius];
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Radius, Arc, Direction];
     }
 }
--- YukkuriMovieMaker.Plugin.Community/Effect/Video/RadialArrangeGroupItemsEffect/RadialArrangeGroupItemsEffectProcessor.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/RadialArrangeGroupItemsEffect/RadialArrangeGroupItemsEffectProcessor.cs
@@ -18,13 +18,18 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RadialArrangeGroupItem
             var fps = effectDescription.FPS;
 
             var radius = item.Radius.GetValue(frame, length, fps);
+            var arc = item.Arc.GetValue(frame, length, fps);
+            var direction = item.Direction.GetValue(frame, length, fps);
             var isRotationSynchronized = item.IsRotationSynchronized;
+            var isCentering = item.IsCentering;
 
             var groupIndex = effectDescription.GroupIndex;
             var groupCount = effectDescription.GroupCount;
             if (groupCount == 0)
                 return effectDescription.DrawDescription;
-            var angle = (float)(2 * Math.PI * groupIndex / groupCount);
+            var angle = (float)(arc / 180 * Math.PI * groupIndex / groupCount + direction / 180 * Math.PI);
+            if(isCentering)
+                angle -= (float)(arc / 180 * (groupCount - 1) / groupCount * Math.PI / 2);
 
             var matrix = Matrix4x4.CreateRotationZ(angle);
             var v = Vector3.Transform(new Vector3(0, (float)-radius, 0), matrix);
@@ -32,7 +37,7 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RadialArrangeGroupItem
             return desc with
             {
                 Draw = desc.Draw + v,
-                Rotation = isRotationSynchronized ? desc.Rotation + new Vector3(0, 0, 360* groupIndex / groupCount) : desc.Rotation,
+                Rotation = isRotationSynchronized ? desc.Rotation + new Vector3(0, 0, (float)(angle / MathF.PI * 180)) : desc.Rotation,
             };
         }
 
