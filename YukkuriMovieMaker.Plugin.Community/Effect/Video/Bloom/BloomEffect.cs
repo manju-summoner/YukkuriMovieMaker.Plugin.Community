44c63dbbbcf48c4f6e6042905835ee7796beebda
manju summoner <manju_summoner@manjubox.net>
2025-06-11 17:21:15 +0900

    「発光」エフェクトを追加

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/Bloom/BloomEffect.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.ItemEditor.CustomVisibilityAttributes;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.Bloom
+{
+    [VideoEffect(nameof(Texts.Bloom), [VideoEffectCategories.Filtering], ["ブルーム", "bloom", "グロー", "glow"], IsAviUtlSupported = false, ResourceType = typeof(Texts))]
+    internal class BloomEffect : VideoEffectBase
+    {
+        public override string Label => Texts.Bloom;
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.Strength), Description = nameof(Texts.StrengthDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "%", 0, 100)]
+        public Animation Strength { get; } = new Animation(100, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.Threshold), Description = nameof(Texts.StrengthDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "%", 0, 100)]
+        public Animation Threshold { get; } = new Animation(50, 0, 100);
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.Blur), Description = nameof(Texts.StrengthDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 250)]
+        public Animation Blur { get; } = new Animation(50, 0, 250);
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.IsFixedSizeEnabled), Description = nameof(Texts.IsFixedSizeEnabledDesc), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsFixedSizeEnabled { get => isFiexedSizeEnabled; set => Set(ref isFiexedSizeEnabled, value); }
+        bool isFiexedSizeEnabled = false;
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.IsColorizationEnabled), Description = nameof(Texts.StrengthDesc), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsColorizationEnabled { get=> isColorizationEnabled; set => Set(ref isColorizationEnabled, value); }
+        bool isColorizationEnabled = false;
+
+        [Display(GroupName = nameof(Texts.Bloom), Name = nameof(Texts.Color), Description = nameof(Texts.StrengthDesc), ResourceType = typeof(Texts))]
+        [ColorPicker]
+        [ShowPropertyEditorWhen(nameof(IsColorizationEnabled), true)]
+        public Color Color { get=> color; set => Set(ref color, value); }
+        Color color = Colors.White;
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            var fps = exoOutputDescription.VideoInfo.FPS;
+            yield return $"_name=アニメーション効果\r\n" +
+                $"_disable={(IsEnabled ? 0 : 1)}\r\n" +
+                $"track0={Strength.ToExoString(keyFrameIndex, "F1", fps)}\r\n" +
+                $"track1={Threshold.ToExoString(keyFrameIndex, "F1", fps)}\r\n" +
+                $"track2={Blur.ToExoString(keyFrameIndex, "F1", fps)}\r\n" +
+                $"name=発光@YMM4-未実装\r\n" +
+                $"param=" +
+                    $"local isColorizationEnabled={(IsColorizationEnabled?1:0)};" +
+                    $"local color={Color.R:X2}{Color.G:X2}{Color.B:X2};" +
+                    $"\r\n";
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new BloomEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Strength, Threshold, Blur];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/Bloom/BloomEffectProcessor.cs
@@ -0,0 +1,143 @@
+﻿using Vortice.Direct2D1;
+using Vortice.Direct2D1.Effects;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Player.Video.Effects;
+using Vortice.Mathematics;
+using Vortice;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.Bloom
+{
+    internal class BloomEffectProcessor(IGraphicsDevicesAndContext devices, BloomEffect item) : VideoEffectProcessorBase(devices)
+    {
+        readonly IGraphicsDevicesAndContext devices = devices;
+
+        LuminanceKey.LuminanceKeyCustomEffect? luminanceKey;
+        ColorMatrix? colorize;
+        GaussianBlur? blur;
+        ColorMatrix? opacity;
+        Composite? composite;
+
+        bool isFirst = true;
+        double strength, threshold, radius;
+        bool isColorizationEnabled, isFixedSizeEnabled;
+        System.Windows.Media.Color color;
+
+        public override DrawDescription Update(EffectDescription desc)
+        {
+            if (luminanceKey is null || blur is null || colorize is null || opacity is null || composite is null)
+                return desc.DrawDescription;
+
+            var frame = desc.ItemPosition.Frame;
+            var length = desc.ItemDuration.Frame;
+            var fps = desc.FPS;
+
+            var strength = item.Strength.GetValue(frame,length, fps) / 100;
+            var threshold = item.Threshold.GetValue(frame, length, fps) / 100;
+            var radius = item.Blur.GetValue(frame, length, fps);
+            var isColorizationEnabled = item.IsColorizationEnabled;
+            var color = item.Color;
+            var isFixedSizeEnabled = item.IsFixedSizeEnabled;
+
+            if (isFirst || this.threshold != threshold)
+                luminanceKey.Threshold = (float)threshold;
+            if (isFirst || this.isColorizationEnabled != isColorizationEnabled || this.color != color)
+            {
+                colorize.Matrix = isColorizationEnabled
+                    ? new Matrix5x4() 
+                    {
+                        M11 = 0, M12 = 0, M13 = 0, M14 = 0,
+                        M21 = 0, M22 = 0, M23 = 0, M24 = 0,
+                        M31 = 0, M32 = 0, M33 = 0, M34 = 0,
+                        M41 = (float)(color.R / 255d * color.A / 255d), M42 = (float)(color.G / 255d * color.A / 255d), M43 = (float)(color.B / 255d * color.A / 255d), M44 = (float)(color.A / 255d),
+                        M51 = 0, M52 = 0, M53 = 0, M54 = 0,
+                    }
+                    : new Matrix5x4() { M11 = 1, M22 = 1, M33 = 1, M44 = 1 };
+            }
+            if(isFirst || this.radius != radius)
+                blur.StandardDeviation = (float)(radius / 3);
+            if (isFirst || this.strength != strength) 
+            { 
+                opacity.Matrix = new Matrix5x4() 
+                {
+                     M11 = (float)strength, M12 = 0, M13 = 0, M14 = 0,
+                     M21 = 0, M22 = (float)strength, M23 = 0, M24 = 0,
+                     M31 = 0, M32 = 0, M33 = (float)strength, M34 = 0,
+                     M41 = 0, M42 = 0, M43 = 0, M44 = (float)strength,
+                     M51 = 0, M52 = 0, M53 = 0, M54 = 0,
+                };
+            }
+            if (isFirst || this.isFixedSizeEnabled != isFixedSizeEnabled)
+                blur.BorderMode = isFixedSizeEnabled ? BorderMode.Hard : BorderMode.Soft;
+
+            isFirst = false;
+            this.strength = strength;
+            this.threshold = threshold;
+            this.radius = radius;
+            this.isColorizationEnabled = isColorizationEnabled;
+            this.color = color;
+            this.isFixedSizeEnabled = isFixedSizeEnabled;
+
+            return desc.DrawDescription;
+        }
+
+        protected override ID2D1Image? CreateEffect(IGraphicsDevicesAndContext devices)
+        {
+            luminanceKey = new LuminanceKey.LuminanceKeyCustomEffect(devices);
+            if(!luminanceKey.IsEnabled)
+            {
+                luminanceKey.Dispose();
+                luminanceKey = null;
+                return null;
+            }
+            luminanceKey.Smoothness = 0;
+            disposer.Collect(luminanceKey);
+
+            colorize = new ColorMatrix(devices.DeviceContext);
+            disposer.Collect(colorize);
+
+            blur = new GaussianBlur(devices.DeviceContext)
+            { 
+                Optimization = GaussianBlurOptimization.Quality
+            };
+            disposer.Collect(blur);
+
+            opacity = new ColorMatrix(devices.DeviceContext);
+            disposer.Collect(opacity);
+
+            composite = new Composite(devices.DeviceContext)
+            { 
+                InputCount = 2,
+                Mode = CompositeMode.Plus,
+            };
+            disposer.Collect(composite);
+
+            using (var output = luminanceKey.Output)
+                colorize.SetInput(0, output, true);
+            using (var output = colorize.Output)
+                blur.SetInput(0, output, true);
+            using (var output = blur.Output)
+                opacity.SetInput(0, output, true);
+            using (var output = opacity.Output)
+                composite.SetInput(1, output, true);
+
+            var result = composite.Output;
+            disposer.Collect(result);
+            return result;
+        }
+
+        protected override void ClearEffectChain()
+        {
+            luminanceKey?.SetInput(0, null, true);
+            blur?.SetInput(0, null, true);
+            composite?.SetInput(0, null, true);
+            composite?.SetInput(1, null, true);
+        }
+
+        protected override void setInput(ID2D1Image? input)
+        {
+            luminanceKey?.SetInput(0, input, true);
+            composite?.SetInput(0, input, true);
+        }
+    }
+}
