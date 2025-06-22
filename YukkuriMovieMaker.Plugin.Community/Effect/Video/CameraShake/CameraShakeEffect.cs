b0d352a342e2b5e1c9cd083fe52c4f64e06c0fb8
べにかずら <155831126+benikazura@users.noreply.github.com>
2025-06-22 20:39:52 +0900

    「手ぶれカメラ」エフェクトを追加

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/CameraShake/CameraShakeEffect.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.CameraShake
+{   
+    [VideoEffect(nameof(Texts.CameraShakeEffect), [VideoEffectCategories.Camera], ["揺れ", "camera shake"], IsAviUtlSupported = false, IsEffectItemSupported = false, ResourceType = typeof(Texts))]
+    internal class CameraShakeEffect : VideoEffectBase
+    {
+        public override string Label => Texts.CameraShakeEffect;
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.X), Description = nameof(Texts.XDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 500)]
+        public Animation X { get; } = new Animation(10, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Y), Description = nameof(Texts.YDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 500)]
+        public Animation Y { get; } = new Animation(10, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Z), Description = nameof(Texts.ZDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 500)]
+        public Animation Z { get; } = new Animation(10, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Yaw), Description = nameof(Texts.YawDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", 0, 360)]
+        public Animation Yaw { get; } = new Animation(5, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Pitch), Description = nameof(Texts.PitchDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", 0, 360)]
+        public Animation Pitch { get; } = new Animation(5, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Roll), Description = nameof(Texts.RollDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", 0, 360)]
+        public Animation Roll { get; } = new Animation(5, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.CameraShakeEffect), Name = nameof(Texts.Span), Description = nameof(Texts.Span), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", "秒", 0, 0.25)]
+        public Animation Span { get; } = new Animation(0.5, 0, YMM4Constants.VeryLargeValue);
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return [];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new CameraShakeEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [X, Y, Z, Pitch, Yaw, Roll, Span];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/CameraShake/CameraShakeEffectProcessor.cs
@@ -0,0 +1,53 @@
+﻿using MathNet.Numerics.Random;
+using System.Numerics;
+using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Player.Video.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.CameraShake
+{
+    internal class CameraShakeEffectProcessor(IGraphicsDevicesAndContext devices, CameraShakeEffect item) : VideoEffectProcessorBase(devices)
+    {
+        public override DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var span = item.Span.GetValue(frame, length, fps);
+            var x = (float)(item.X.GetValue(frame, length, fps) * GetCenteringRandomValue(span, frame, fps, 0));
+            var y = (float)(item.Y.GetValue(frame, length, fps) * GetCenteringRandomValue(span, frame, fps, 1));
+            var z = (float)(item.Z.GetValue(frame, length, fps) * GetCenteringRandomValue(span, frame, fps, 2));
+            var yaw = (float)(item.Yaw.GetValue(frame, length, fps) / 180 * Math.PI * GetCenteringRandomValue(span, frame, fps, 3));
+            var pitch = (float)(item.Pitch.GetValue(frame, length, fps) / 180 * Math.PI * GetCenteringRandomValue(span, frame, fps, 4));
+            var roll = (float)(item.Roll.GetValue(frame, length, fps) / 180 * Math.PI * GetCenteringRandomValue(span, frame, fps, 5));
+
+            var camera = Matrix4x4.CreateTranslation(x, y, z) * Matrix4x4.CreateTranslation(0, 0, -1000) * Matrix4x4.CreateFromYawPitchRoll(yaw, pitch, roll) * Matrix4x4.CreateTranslation(0, 0, 1000);
+
+            return effectDescription.DrawDescription with
+            {
+                Camera = effectDescription.DrawDescription.Camera * camera
+            };
+        }
+
+        private double GetCenteringRandomValue(double span, int frame, int fps, int parameterID)
+        {
+            double random = span != 0.0 ? Animation.GetRandomMoveRate(item, parameterID, frame, fps, span) : new MersenneTwister(this.GetHashCode() / (parameterID + 1) + frame).NextDouble();
+            return random - 0.5;
+        }
+
+        protected override ID2D1Image? CreateEffect(IGraphicsDevicesAndContext devices)
+        {
+            return null;
+        }
+
+        protected override void setInput(ID2D1Image? input)
+        {
+        }
+
+        protected override void ClearEffectChain()
+        {
+        }
+    }
+}
