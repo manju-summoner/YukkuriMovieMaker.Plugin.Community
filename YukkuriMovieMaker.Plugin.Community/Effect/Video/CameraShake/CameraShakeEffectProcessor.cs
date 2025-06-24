15238a2f62f01a94c9908dd41236ae4cfe04e3eb
manju summoner <manju_summoner@manjubox.net>
2025-06-24 23:30:57 +0900

    改行を追加

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/CameraShake/CameraShakeEffectProcessor.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/CameraShake/CameraShakeEffectProcessor.cs
@@ -24,7 +24,12 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.CameraShake
             var roll = (float)(item.Roll.GetValue(frame, length, fps) / 180 * Math.PI * GetCenteringRandomValue(span, frame, fps, 5));
 
             var drawDescription = effectDescription.DrawDescription;
-            var camera = drawDescription.Camera * Matrix4x4.CreateTranslation(x, y, z) * Matrix4x4.CreateRotationY(yaw, new(0, 0, 1000)) * Matrix4x4.CreateRotationX(pitch, new(0, 0, 1000)) * Matrix4x4.CreateRotationZ(roll, new(0, 0, 1000));
+            var camera = 
+                drawDescription.Camera 
+                * Matrix4x4.CreateTranslation(x, y, z)
+                * Matrix4x4.CreateRotationY(yaw, new(0, 0, 1000))
+                * Matrix4x4.CreateRotationX(pitch, new(0, 0, 1000))
+                * Matrix4x4.CreateRotationZ(roll, new(0, 0, 1000));
 
             return drawDescription with { Camera = camera };
         }
