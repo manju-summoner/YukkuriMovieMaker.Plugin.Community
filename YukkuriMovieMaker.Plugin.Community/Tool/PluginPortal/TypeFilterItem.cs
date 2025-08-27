80200467e9f6187c09ce4b547baa56a345351818
manju summoner <manju_summoner@manjubox.net>
2025-08-28 04:03:07 +0900

    プライマリーコンストラクタ化

--- YukkuriMovieMaker.Plugin.Community/Tool/PluginPortal/TypeFilterItem.cs
+++ YukkuriMovieMaker.Plugin.Community/Tool/PluginPortal/TypeFilterItem.cs
@@ -2,31 +2,22 @@
 
 namespace YukkuriMovieMaker.Plugin.Community.Tool.PluginPortal
 {
-    public class TypeFilterItem : INotifyPropertyChanged
+    public class TypeFilterItem(string name, bool isChecked, Action filterAction) : INotifyPropertyChanged
     {
-        private bool _isChecked;
-        private readonly Action _filterAction;
 
-        public string Name { get; }
+        public string Name { get; } = name;
         public bool IsChecked
         {
-            get => _isChecked;
+            get => isChecked;
             set
             {
-                if (_isChecked != value)
+                if (isChecked != value)
                 {
-                    _isChecked = value;
+                    isChecked = value;
                     OnPropertyChanged(nameof(IsChecked));
-                    _filterAction?.Invoke();
+                    filterAction?.Invoke();
                 }
             }
-        }
-
-        public TypeFilterItem(string name, bool isChecked, Action filterAction)
-        {
-            Name = name;
-            _isChecked = isChecked;
-            _filterAction = filterAction;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
