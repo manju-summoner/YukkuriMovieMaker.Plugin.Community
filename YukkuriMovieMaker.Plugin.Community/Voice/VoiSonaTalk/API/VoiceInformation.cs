fbbf1ac8cf4daca02ffc9b457fb3272da012eb90
manju summoner <manju_summoner@manjubox.net>
2025-12-04 17:16:52 +0900

    VoiSonaTalk v1.3.5のAPIの変更に対応

--- YukkuriMovieMaker.Plugin.Community/Voice/VoiSonaTalk/API/VoiceInformation.cs
+++ YukkuriMovieMaker.Plugin.Community/Voice/VoiSonaTalk/API/VoiceInformation.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 namespace YukkuriMovieMaker.Plugin.Community.Voice.VoiSonaTalk.API
 {
     internal record VoiceInformation(
-        [property: JsonProperty("display_name")] DisplayName[] DisplayName,
+        [property: JsonProperty("display_names")] DisplayName[] DisplayNames,
         [property: JsonProperty("languages")] string[] Languages,
         [property: JsonProperty("voice_name")] string VoiceName,
         [property: JsonProperty("voice_version")] string VoiceVersion,
@@ -13,8 +13,11 @@ namespace YukkuriMovieMaker.Plugin.Community.Voice.VoiSonaTalk.API
     {
         public string GetDisplayName(CultureInfo cultureInfo)
         {
+            if (DisplayNames is null || DisplayNames.Length is 0)
+                return Texts.FailedToGetDisplayNameMessage;
+
             // 完全に一致するものを探す
-            foreach (var displayName in DisplayName)
+            foreach (var displayName in DisplayNames)
             {
                 if (cultureInfo.Name.Equals(displayName.Language.Replace('_', '-'), StringComparison.OrdinalIgnoreCase))
                     return displayName.Name;
@@ -22,15 +25,15 @@ namespace YukkuriMovieMaker.Plugin.Community.Voice.VoiSonaTalk.API
 
             // 言語コードだけで一致するものを探す
             var langCode = cultureInfo.Name.Split('-')[0];
-            foreach (var displayName in DisplayName)
+            foreach (var displayName in DisplayNames)
             {
                 if (langCode.Equals(displayName.Language.Split('_')[0], StringComparison.OrdinalIgnoreCase))
                     return displayName.Name;
             }
 
             // 一致する言語が存在しない場合は最初の表示名を返す
-            if(DisplayName.Length > 0)
-                return DisplayName[0].Name;
+            if(DisplayNames.Length > 0)
+                return DisplayNames[0].Name;
 
             // 表示名が存在しない場合は素の名前を返す
             return VoiceName;
