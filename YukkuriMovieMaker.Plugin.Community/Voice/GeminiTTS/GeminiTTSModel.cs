5b77e93dd11da1f16fdfe8cebd553f626322cc64
manju summoner <manju_summoner@manjubox.net>
2026-04-19 06:11:36 +0900

    gemini-3.1-flash-tts-previewに対応

--- YukkuriMovieMaker.Plugin.Community/Voice/GeminiTTS/GeminiTTSModel.cs
+++ YukkuriMovieMaker.Plugin.Community/Voice/GeminiTTS/GeminiTTSModel.cs
@@ -11,7 +11,8 @@ namespace YukkuriMovieMaker.Plugin.Community.Voice.GeminiTTS
         public static string DefaultModelName = "gemini-2.5-flash-preview-tts";
         public static GeminiTTSModel[] Models = [
             new GeminiTTSModel("gemini-2.5-pro-preview-tts", new RateLimits(0, 10,100,100)),
-            new GeminiTTSModel("gemini-2.5-flash-preview-tts", new RateLimits(3, 10,1000,1000))
+            new GeminiTTSModel("gemini-2.5-flash-preview-tts", new RateLimits(3, 10,1000,1000)),
+            new GeminiTTSModel("gemini-3.1-flash-tts-preview", new RateLimits(3, 10,1000,1000))
         ];
         public static GeminiTTSModel? GetModel(string name) => Models.FirstOrDefault(m => m.Name == name);
         public static GeminiTTSModel DefaultModel => GetModel(DefaultModelName) ?? Models[0];
