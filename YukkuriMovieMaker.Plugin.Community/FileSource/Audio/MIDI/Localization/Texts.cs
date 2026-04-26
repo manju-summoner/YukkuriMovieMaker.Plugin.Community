8d726fde0e0dba76b8a1a239956c666c721ac303
routersys <github.routersys@gmail.com>
2026-04-26 10:27:10 +0900

    Texts.csの非統一性を解消

--- YukkuriMovieMaker.Plugin.Community/FileSource/Audio/MIDI/Localization/Texts.cs
+++ YukkuriMovieMaker.Plugin.Community/FileSource/Audio/MIDI/Localization/Texts.cs
@@ -3,4 +3,7 @@ using YukkuriMovieMaker.Generator;
 namespace YukkuriMovieMaker.Plugin.Community.FileSource.Audio.MIDI.Localization;
 
 [AutoGenLocalizer]
-partial class Texts;
+partial class Texts
+{
+
+}
