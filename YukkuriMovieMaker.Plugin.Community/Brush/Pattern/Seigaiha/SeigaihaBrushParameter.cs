1b576e28b56ac4a1af59f2f2ced83043583b1b7b
manju summoner <manju_summoner@manjubox.net>
2026-05-15 04:52:26 +0900

    グルーピングを削除

--- YukkuriMovieMaker.Plugin.Community/Brush/Pattern/Seigaiha/SeigaihaBrushParameter.cs
+++ YukkuriMovieMaker.Plugin.Community/Brush/Pattern/Seigaiha/SeigaihaBrushParameter.cs
@@ -8,35 +8,35 @@ namespace YukkuriMovieMaker.Plugin.Community.Brush.Pattern.Seigaiha
 {
     internal class SeigaihaBrushParameter : DrawingBrushParameterBase
     {
-        [Display(GroupName = nameof(Texts.FlatColorGroup), Name = nameof(Texts.GradientEnabled), ResourceType = typeof(Texts), Order = 0)]
+        [Display(Name = nameof(Texts.GradientEnabled), ResourceType = typeof(Texts))]
         [ToggleSlider]
         public bool GradientEnabled { get => gradientEnabled; set => Set(ref gradientEnabled, value); }
         bool gradientEnabled = false;
 
-        [Display(GroupName = nameof(Texts.FlatColorGroup), Name = nameof(Texts.Color), ResourceType = typeof(Texts), Order = 1)]
+        [Display(Name = nameof(Texts.Color), ResourceType = typeof(Texts))]
         [ColorPicker]
         [FlatColorVisible]
         public Color Color { get => color; set => Set(ref color, value); }
         Color color = Colors.White;
 
-        [Display(GroupName = nameof(Texts.GradientColorGroup), Name = nameof(Texts.OuterColor), ResourceType = typeof(Texts), Order = 2)]
+        [Display(Name = nameof(Texts.OuterColor), ResourceType = typeof(Texts))]
         [ColorPicker]
         [GradientColorVisible]
         public Color OuterColor { get => outerColor; set => Set(ref outerColor, value); }
         Color outerColor = Colors.White;
 
-        [Display(GroupName = nameof(Texts.GradientColorGroup), Name = nameof(Texts.InnerColor), ResourceType = typeof(Texts), Order = 3)]
+        [Display(Name = nameof(Texts.InnerColor), ResourceType = typeof(Texts))]
         [ColorPicker]
         [GradientColorVisible]
         public Color InnerColor { get => innerColor; set => Set(ref innerColor, value); }
         Color innerColor = Colors.LightBlue;
 
-        [Display(GroupName = nameof(Texts.CommonColorGroup), Name = nameof(Texts.BackgroundColor), ResourceType = typeof(Texts), Order = 4)]
+        [Display(Name = nameof(Texts.BackgroundColor), ResourceType = typeof(Texts))]
         [ColorPicker]
         public Color BackgroundColor { get => backgroundColor; set => Set(ref backgroundColor, value); }
         Color backgroundColor = Colors.Black;
 
-        [Display(GroupName = nameof(Texts.CommonColorGroup), Name = nameof(Texts.StrokeColor), ResourceType = typeof(Texts), Order = 5)]
+        [Display(Name = nameof(Texts.StrokeColor), ResourceType = typeof(Texts))]
         [ColorPicker]
         public Color StrokeColor { get => strokeColor; set => Set(ref strokeColor, value); }
         Color strokeColor = Colors.Gray;
