c2f02a4450e094b5e11013a5b3fc40db6aad17e0
routersys <github.routersys@gmail.com>
2026-05-15 21:10:49 +0900

    ダイアログViewModelのライフサイクル管理を統一

--- YukkuriMovieMaker.Plugin.Community/Tool/Notepad/NotepadViewModel.cs
+++ YukkuriMovieMaker.Plugin.Community/Tool/Notepad/NotepadViewModel.cs
@@ -161,7 +161,6 @@ namespace YukkuriMovieMaker.Plugin.Community.Tool.Notepad
                         MessageBoxButton.OK,
                         MessageBoxImage.Information));
             });
-            ClearImageCacheViewModel = null;
         }
 
         public void LoadState(ToolState stateData)
