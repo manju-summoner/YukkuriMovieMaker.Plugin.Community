4677874901386eeb367e0eaffd0ecaf9cf3ca850
hazimeyou <125953620+hazimeyou@users.noreply.github.com>
2026-05-21 21:24:09 +0900

    MIDIを元に戻しました

--- YukkuriMovieMaker.Plugin.Community/FileSource/Audio/MIDI/ViewModels/MidiSettingsViewModel.cs
+++ YukkuriMovieMaker.Plugin.Community/FileSource/Audio/MIDI/ViewModels/MidiSettingsViewModel.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using YukkuriMovieMaker.Commons;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using YukkuriMovieMaker.Plugin.Community.FileSource.Audio.MIDI.Models;
 using YukkuriMovieMaker.Plugin.Community.FileSource.Audio.MIDI.Services;
 
 namespace YukkuriMovieMaker.Plugin.Community.FileSource.Audio.MIDI.ViewModels;
 
-internal sealed class MidiSettingsViewModel : Bindable
+internal sealed class MidiSettingsViewModel : INotifyPropertyChanged
 {
     public MidiPluginSettings Settings => MidiPluginSettings.Default;
     public AudioSettings Audio => Settings.Audio;
@@ -20,20 +19,15 @@ internal sealed class MidiSettingsViewModel : Bindable
     public ObservableCollection<string> InstalledSoundFonts { get; } = [];
     public IReadOnlyList<RenderingMode> AvailableRenderingModes { get; } = Enum.GetValues<RenderingMode>();
 
-    public ActionCommand AddLayerCommand { get; }
-    public ActionCommand RemoveLayerCommand { get; }
-    public ActionCommand RefreshSoundFontsCommand { get; }
+    public ICommand AddLayerCommand { get; }
+    public ICommand RemoveLayerCommand { get; }
+    public ICommand RefreshSoundFontsCommand { get; }
 
-    private SoundFontEntry? selectedLayer;
+    private SoundFontEntry? _selectedLayer;
     public SoundFontEntry? SelectedLayer
     {
-        get => selectedLayer;
-        set
-        {
-            if (!Set(ref selectedLayer, value))
-                return;
-            RemoveLayerCommand.RaiseCanExecuteChanged();
-        }
+        get => _selectedLayer;
+        set => SetField(ref _selectedLayer, value);
     }
 
     public bool HasSoundFonts => InstalledSoundFonts.Count > 0;
@@ -41,13 +35,13 @@ internal sealed class MidiSettingsViewModel : Bindable
 
     public MidiSettingsViewModel()
     {
-        AddLayerCommand = new ActionCommand(_ => true, _ => SoundFont.Layers.Add(new SoundFontEntry()));
-        RemoveLayerCommand = new ActionCommand(_ => SelectedLayer is not null, _ =>
+        AddLayerCommand = new RelayCommand(() => SoundFont.Layers.Add(new SoundFontEntry()));
+        RemoveLayerCommand = new RelayCommand(() =>
         {
             if (SelectedLayer is not null)
                 SoundFont.Layers.Remove(SelectedLayer);
-        });
-        RefreshSoundFontsCommand = new ActionCommand(_ => true, _ => RefreshList());
+        }, () => SelectedLayer is not null);
+        RefreshSoundFontsCommand = new RelayCommand(RefreshList);
         RefreshList();
     }
 
@@ -65,6 +59,30 @@ internal sealed class MidiSettingsViewModel : Bindable
 
         OnPropertyChanged(nameof(HasSoundFonts));
         OnPropertyChanged(nameof(HasNoSoundFonts));
-        RemoveLayerCommand.RaiseCanExecuteChanged();
+        CommandManager.InvalidateRequerySuggested();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        return true;
+    }
+}
+
+file sealed class RelayCommand(Action execute, Func<bool>? canExecute = null) : ICommand
+{
+    public bool CanExecute(object? _) => canExecute?.Invoke() ?? true;
+    public void Execute(object? _) => execute();
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
     }
 }
