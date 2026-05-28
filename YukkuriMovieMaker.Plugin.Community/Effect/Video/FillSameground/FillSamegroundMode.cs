683a42fa0bbf1b0b2b7b775b15697e911b862d35
tenContΔ <tenkonta@gmail.com>
2026-05-28 19:41:02 +0900

    Update FillSamegroundMode.cs

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/FillSameground/FillSamegroundMode.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/FillSameground/FillSamegroundMode.cs
@@ -5,13 +5,13 @@ namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.FillSameground
     [Flags]
     public enum FillSamegroundMode
     {
-        [Display(Name = "位置指定")]
+        [Display(Name = nameof(Texts.FillSamegroundModePositionName), ResourceType = typeof(Texts))]
         Position = 1,
 
-        [Display(Name = "色指定")]
+        [Display(Name = nameof(Texts.FillSamegroundModeColorName), ResourceType = typeof(Texts))]
         Color = 2,
 
-        [Display(Name = "指定した位置の色指定")]
+        [Display(Name = nameof(Texts.FillSamegroundModePositionColorName), ResourceType = typeof(Texts))]
         PositionColor = 4,
     }
 }
