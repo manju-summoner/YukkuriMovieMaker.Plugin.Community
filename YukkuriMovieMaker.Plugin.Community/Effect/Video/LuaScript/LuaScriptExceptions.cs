8ee7a29f8344ae6b04b44921dad2b855b4f2919c
routersys <github.routersys@gmail.com>
2026-05-31 10:48:42 +0900

    例外クラスをプライマリコンストラクタで統一

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/LuaScript/LuaScriptExceptions.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/LuaScript/LuaScriptExceptions.cs
@@ -1,20 +1,8 @@
 namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.LuaScript
 {
-    internal abstract class LuaScriptException : Exception
-    {
-        protected LuaScriptException(string message) : base(message) { }
-        protected LuaScriptException(string message, Exception inner) : base(message, inner) { }
-    }
+    internal abstract class LuaScriptException(string message, Exception? inner = null) : Exception(message, inner);
 
-    internal sealed class LuaScriptCompilationException : LuaScriptException
-    {
-        public LuaScriptCompilationException(string message) : base(message) { }
-        public LuaScriptCompilationException(string message, Exception inner) : base(message, inner) { }
-    }
+    internal sealed class LuaScriptCompilationException(string message, Exception? inner = null) : LuaScriptException(message, inner);
 
-    internal sealed class LuaScriptRuntimeException : LuaScriptException
-    {
-        public LuaScriptRuntimeException(string message) : base(message) { }
-        public LuaScriptRuntimeException(string message, Exception inner) : base(message, inner) { }
-    }
+    internal sealed class LuaScriptRuntimeException(string message, Exception? inner = null) : LuaScriptException(message, inner);
 }
