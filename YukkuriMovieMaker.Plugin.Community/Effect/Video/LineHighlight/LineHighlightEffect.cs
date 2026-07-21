62a662af5e71ff09398947f37261ec2e736a1d3e
べにかずら <155831126+benikazura@users.noreply.github.com>
2026-07-21 10:43:49 +0900

    「ラインハイライト」エフェクトを追加

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/LineHighlight/LineHighlightEffect.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Windows.Media;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.ItemEditor.CustomVisibilityAttributes;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.LineHighlight
+{
+    [VideoEffect(nameof(Texts.LineHighlight), [VideoEffectCategories.Animation], ["line highlight"], IsAviUtlSupported = false, ResourceType = typeof(Texts))]
+    internal class LineHighlightEffect : VideoEffectBase
+    {
+        public override string Label => Texts.LineHighlight;
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Color), Description = nameof(Texts.ColorDesc), ResourceType = typeof(Texts))]
+        [ColorPicker]
+        public Color Color { get => color; set => Set(ref color, value); }
+        Color color = Colors.White;
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Strength), Description = nameof(Texts.Strength), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "%", 0, 100)]
+        public Animation Strength { get; } = new Animation(100, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Fade), Description = nameof(Texts.FadeDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "%", 0, 100)]
+        public Animation Fade { get; } = new Animation(50, 0, 100);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Size), Description = nameof(Texts.SizeDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 500)]
+        public Animation Size { get; } = new Animation(100, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Blur), Description = nameof(Texts.BlurDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "px", 0, 100)]
+        public Animation Blur { get; } = new Animation(50, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.Angle), Description = nameof(Texts.AngleDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation Angle { get; } = new Animation(-45, YMM4Constants.VerySmallValue, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.EasingType), Description = nameof(Texts.EasingTypeDesc), ResourceType = typeof(Texts))]
+        [EnumComboBox]
+        public EasingType EasingType { get => easingType; set => Set(ref easingType, value); }
+        EasingType easingType = EasingType.Expo;
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.EasingMode), Description = nameof(Texts.EasingModeDesc), ResourceType = typeof(Texts))]
+        [EnumComboBox]
+        public EasingMode EasingMode { get => easingMode; set => Set(ref easingMode, value); }
+        EasingMode easingMode = EasingMode.Out;
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.EffectDuration), Description = nameof(Texts.EffectDurationDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.SecUnit), 0, 1, ResourceType = typeof(Texts))]
+        public Animation EffectDuration { get; } = new Animation(1, 0, YMM4Constants.VeryLargeValue);
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.IsLoop), Description = nameof(Texts.IsLoopDesc), ResourceType = typeof(Texts))]
+        [ToggleSlider]
+        public bool IsLoop { get => isLoop; set => Set(ref isLoop, value); }
+        bool isLoop = false;
+
+        [Display(GroupName = nameof(Texts.LineHighlight), Name = nameof(Texts.LoopInterval), Description = nameof(Texts.LoopIntervalDesc), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.SecUnit), 0, 1, ResourceType = typeof(Texts))]
+        [ShowPropertyEditorWhen(nameof(IsLoop), true)]
+        public Animation LoopInterval { get; } = new Animation(1, 0, YMM4Constants.VeryLargeValue);
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return [];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new LineHighlightEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Strength, Fade, Size, Blur, Angle, EffectDuration, LoopInterval];
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/LineHighlight/LineHighlightEffectProcessor.cs
@@ -0,0 +1,230 @@
+﻿using System.Numerics;
+using Vortice;
+using Vortice.Direct2D1;
+using Vortice.Direct2D1.Effects;
+using Vortice.Mathematics;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Player.Video.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.LineHighlight
+{
+    using AlphaMask = Vortice.Direct2D1.Effects.AlphaMask;
+
+    internal class LineHighlightEffectProcessor(IGraphicsDevicesAndContext devices, LineHighlightEffect item) : VideoEffectProcessorBase(devices)
+    {
+        readonly IGraphicsDevicesAndContext devices = devices;
+        readonly LineHighlightEffect item = item;
+
+        ID2D1CommandList? commandList;
+        AlphaMask? alphaMask;
+        ColorMatrix? colorMatrix;
+        Composite? composite;
+
+        bool isFirst = true;
+        RawRectF bounds;
+        System.Windows.Media.Color color;
+        double strength, fade, size, blur, angle, animationRate;
+        bool isAnimationActive;
+
+        public override DrawDescription Update(EffectDescription effectDescription)
+        {
+            if (IsPassThroughEffect
+                || alphaMask is null
+                || colorMatrix is null
+                || composite is null
+                || input is null)
+                return effectDescription.DrawDescription;
+
+            var dc = devices.DeviceContext;
+
+            var bounds = dc.GetImageLocalBounds(input);
+
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var color = item.Color;
+            var strength = item.Strength.GetValue(frame, length, fps) / 100;
+            var fade = item.Fade.GetValue(frame, length, fps) / 100;
+            var size = item.Size.GetValue(frame, length, fps);
+            var blur = item.Blur.GetValue(frame, length, fps);
+            var angle = item.Angle.GetValue(frame, length, fps) / 180 * Math.PI;
+            var easingType = item.EasingType;
+            var easingMode = item.EasingMode;
+            var effectDuration = item.EffectDuration.GetValue(frame, length, fps);
+            var isLoop = item.IsLoop;
+            var loopInterval = item.LoopInterval.GetValue(frame, length, fps);
+
+            var animationPosition = isLoop ? effectDescription.ItemPosition.Time.TotalSeconds % (effectDuration + loopInterval) 
+                : effectDescription.ItemPosition.Time.TotalSeconds;
+            var isAnimationActive = animationPosition < effectDuration;
+            var animationRate = Easing.GetValue(easingType, easingMode, Math.Min(animationPosition / effectDuration, 1));
+
+            if (isFirst || ((this.isAnimationActive == true || isAnimationActive == true) && (!this.bounds.Equals(bounds) || this.color != color
+                || this.strength != strength || this.size != size || this.blur != blur || this.angle != angle || this.animationRate != animationRate)))
+            {
+                if (commandList is not null)
+                {
+                    disposer.RemoveAndDispose(ref commandList);
+                }
+                commandList = dc.CreateCommandList();
+                disposer.Collect(commandList);
+
+                if (isAnimationActive)
+                {
+                    var rotationMatrix = Matrix3x2.CreateRotation((float)angle);
+                    var reverseRotationMatrix = Matrix3x2.CreateRotation(-(float)angle);
+                    Vector2[] edges = [
+                            new Vector2(bounds.Left, bounds.Top),
+                            new Vector2(bounds.Right, bounds.Top),
+                            new Vector2(bounds.Left, bounds.Bottom),
+                            new Vector2(bounds.Right, bounds.Bottom)
+                        ];
+                    var rotedEdges = edges.Select(x => Vector2.Transform(x, reverseRotationMatrix));
+                    float moveBegin = rotedEdges.Min(x => x.Y);
+                    float moveEnd = rotedEdges.Max(x => x.Y);
+                    var moveMatrix = Matrix3x2.CreateTranslation(
+                        new Vector2((float)((moveBegin - (size / 2 + blur / 2)) * (1 - animationRate) + (moveEnd + (size / 2 + blur / 2)) * animationRate)));
+                    double fadeRate = animationRate < 0.5 ? animationRate * 2 : 1 - (animationRate - 0.5) * 2;
+                    float currentFade = (float)(fadeRate + (1 - fadeRate) * fade);
+
+                    var gradientBrushProperty = new LinearGradientBrushProperties()
+                    {
+                        StartPoint = new Vector2(0, -(float)(size / 2 + blur / 2)),
+                        EndPoint = new Vector2(0, (float)(size / 2 + blur / 2))
+                    };
+                    var brushProperties = new BrushProperties(currentFade, moveMatrix * rotationMatrix);
+                    GradientStop[] stops;
+                    if (size >= blur)
+                    {
+                        stops = [
+                            new GradientStop()
+                            {
+                                Position = 0,
+                                Color = new Color4(color.R, color.G, color.B, 0)
+                            },
+                            new GradientStop(){
+                                Position = (float)(blur / size * 0.5),
+                                Color = new Color4(color.R, color.G, color.B, color.A)
+                            },
+                            new GradientStop(){
+                                Position = (float)(1 - blur / size * 0.5),
+                                Color = new Color4(color.R, color.G, color.B, color.A)
+                            },
+                            new GradientStop()
+                            {
+                                Position = 1,
+                                Color = new Color4(color.R, color.G, color.B, 0)
+                            }
+                        ];
+                    }
+                    else
+                    {
+                        stops = [
+                            new GradientStop()
+                            {
+                                Position = 0,
+                                Color = new Color4(color.R, color.G, color.B, 0)
+                            },
+                            new GradientStop(){
+                                Position = 0.5f,
+                                Color = new Color4(color.R, color.G, color.B, color.A * (float)(size / blur))
+                            },
+                            new GradientStop()
+                            {
+                                Position = 1,
+                                Color = new Color4(color.R, color.G, color.B, 0)
+                            }
+                        ];
+                    }
+                    using var gradientStopCollection = dc.CreateGradientStopCollection(stops, ExtendMode.Clamp);
+                    using var brush = dc.CreateLinearGradientBrush(gradientBrushProperty, brushProperties, gradientStopCollection);
+
+                    dc.Target = commandList;
+                    dc.BeginDraw();
+                    dc.FillRectangle(bounds, brush);
+                    dc.EndDraw();
+                    dc.Target = null;
+                    commandList.Close();
+                }
+                else
+                {
+                    dc.Target = commandList;
+                    dc.BeginDraw();
+                    dc.EndDraw();
+                    dc.Target = null;
+                    commandList.Close();
+                }
+
+                colorMatrix.SetInput(0, commandList, true);
+            }
+
+            if (isFirst || this.strength != strength) {
+                colorMatrix.Matrix = new Matrix5x4()
+                {
+                    M11 = (float)strength, M12 = 0, M13 = 0, M14 = 0,
+                    M21 = 0, M22 = (float)strength, M23 = 0, M24 = 0,
+                    M31 = 0, M32 = 0, M33 = (float)strength, M34 = 0,
+                    M41 = 0, M42 = 0, M43 = 0, M44 = (float)strength,
+                    M51 = 0, M52 = 0, M53 = 0, M54 = 0,
+                };
+            }
+
+            isFirst = false;
+            this.bounds = bounds;
+            this.color = color;
+            this.strength = strength;
+            this.fade = fade;
+            this.size = size;
+            this.blur = blur;
+            this.angle = angle;
+            this.isAnimationActive = isAnimationActive;
+            this.animationRate = animationRate;
+
+            return effectDescription.DrawDescription;
+        }
+
+        protected override ID2D1Image? CreateEffect(IGraphicsDevicesAndContext devices)
+        {
+            var dc = devices.DeviceContext;
+
+            colorMatrix = new ColorMatrix(dc);
+            disposer.Collect(colorMatrix);
+
+            alphaMask = new AlphaMask(dc);
+            disposer.Collect(alphaMask);
+
+            composite = new Composite(dc)
+            {
+                InputCount = 2,
+                Mode = CompositeMode.Plus
+            };
+            disposer.Collect(composite);
+
+            using (var output = colorMatrix.Output)
+                alphaMask.SetInput(0, output, true);
+            using (var output = alphaMask.Output)
+                composite.SetInput(1, output, true);
+
+            var result = composite.Output;
+            disposer.Collect(result);
+            return result;
+        }
+
+        protected override void setInput(ID2D1Image? input)
+        {
+            alphaMask?.SetInput(1, input, true);
+            composite?.SetInput(0, input, true);
+        }
+
+        protected override void ClearEffectChain()
+        {
+            colorMatrix?.SetInput(0, null, true);
+            alphaMask?.SetInput(0, null, true);
+            alphaMask?.SetInput(1, null, true);
+            composite?.SetInput(0, null, true);
+            composite?.SetInput(1, null, true);
+        }
+    }
+}
