aa7979ce7831e44d923a1d0c0a18ee0a4f19f2c6
HAIsanGames <229576095+HAIsanGames813@users.noreply.github.com>
2026-07-29 15:28:59 +0900

    映像エフェクト「指定秒間回転」

---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/RotationPerSecond/RotationPerSecond.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RotationPerSecond
+{
+    [VideoEffect(nameof(Texts.rotate_for_seconds), [VideoEffectCategories.Animation,], ["指定秒間回転", "指定秒数回転", "回転", "秒", "指定", "Rotate for Specified Seconds", "Rotate", "Seconds", "Specified"], isAviUtlSupported: false, ResourceType = typeof(Texts))]
+    internal class RotationPerSecond : VideoEffectBase
+    {
+        public override string Label => Texts.rotate_for_seconds;
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.seconds), Description = nameof(Texts.rotation_period), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.unit_seconds), 0.00, 60, ResourceType = typeof(Texts))]
+        public Animation Seconds { get; } = new Animation(1.0, 0.00, 100000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.axis_x), Description = nameof(Texts.x_rotations), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.unit_times), -100, 100, ResourceType = typeof(Texts))]
+        public Animation RotationXCount { get; } = new Animation(0.0, -10000000, 10000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.axis_y), Description = nameof(Texts.y_rotations), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.unit_times), -100, 100, ResourceType = typeof(Texts))]
+        public Animation RotationYCount { get; } = new Animation(0.0, -10000000, 10000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.axis_z), Description = nameof(Texts.z_rotations), ResourceType = typeof(Texts))]
+        [AnimationSlider("F2", nameof(Texts.unit_times), -100, 100, ResourceType = typeof(Texts))]
+        public Animation RotationZCount { get; } = new Animation(0.0, -10000000, 10000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.x_offset), Description = nameof(Texts.x_initial_angle_offset), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation OffsetX { get; } = new Animation(0.0, -360000000, 360000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.y_offset), Description = nameof(Texts.y_initial_angle_offset), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation OffsetY { get; } = new Animation(0.0, -360000000, 360000000);
+
+        [Display(GroupName = nameof(Texts.rotate_for_seconds), Name = nameof(Texts.z_offset), Description = nameof(Texts.z_initial_angle_offset), ResourceType = typeof(Texts))]
+        [AnimationSlider("F1", "°", -360, 360)]
+        public Animation OffsetZ { get; } = new Animation(0.0, -360000000, 360000000);
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() =>
+        [
+            Seconds,
+            RotationXCount, RotationYCount, RotationZCount,
+            OffsetX, OffsetY, OffsetZ,
+        ];
+
+        public override IEnumerable<string> CreateExoVideoFilters(
+            int keyFrameIndex,
+            ExoOutputDescription exoOutputDescription)
+            => [];
+
+        public override IVideoEffectProcessor CreateVideoEffect(
+            IGraphicsDevicesAndContext devices)
+            => new RotationPerSecondProcessor(this);
+    }
+}
---
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/RotationPerSecond/RotationPerSecondProcessor.cs
@@ -0,0 +1,80 @@
+﻿using Vortice.Direct2D1;
+using YukkuriMovieMaker.Player.Video;
+
+namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.RotationPerSecond
+{
+    internal class RotationPerSecondProcessor : IVideoEffectProcessor
+    {
+        readonly RotationPerSecond effect;
+
+        ID2D1Image? input;
+
+        int cachedFrame = -1;
+        double accumX, accumY, accumZ;
+
+        public ID2D1Image Output =>
+            input ?? throw new System.InvalidOperationException("Input が設定されていません。");
+
+        public RotationPerSecondProcessor(RotationPerSecond effect)
+        {
+            this.effect = effect;
+        }
+
+        public void SetInput(ID2D1Image? input) => this.input = input;
+        public void ClearInput() => input = null;
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            int startFrame;
+            if (cachedFrame < 0 || frame < cachedFrame)
+            {
+
+                accumX = effect.OffsetX.GetValue(0, length, fps);
+                accumY = effect.OffsetY.GetValue(0, length, fps);
+                accumZ = effect.OffsetZ.GetValue(0, length, fps);
+                startFrame = 1;
+            }
+            else
+            {
+                startFrame = cachedFrame + 1;
+            }
+
+            for (int i = startFrame; i <= frame; i++)
+            {
+                var seconds = effect.Seconds.GetValue(i, length, fps);
+                var cycleFrames = seconds * fps;
+
+                if (cycleFrames > 0.0)
+                {
+                    accumX += effect.RotationXCount.GetValue(i, length, fps) * 360.0 / cycleFrames;
+                    accumY += effect.RotationYCount.GetValue(i, length, fps) * 360.0 / cycleFrames;
+                    accumZ += effect.RotationZCount.GetValue(i, length, fps) * 360.0 / cycleFrames;
+                }
+            }
+
+            cachedFrame = frame;
+
+            var desc = effectDescription.DrawDescription;
+            var oldRot = desc.Rotation;
+
+            return desc with
+            {
+                Rotation = oldRot with
+                {
+                    X = (float)(oldRot.X + accumX),
+                    Y = (float)(oldRot.Y + accumY),
+                    Z = (float)(oldRot.Z + accumZ),
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+            input = null;
+        }
+    }
+}
