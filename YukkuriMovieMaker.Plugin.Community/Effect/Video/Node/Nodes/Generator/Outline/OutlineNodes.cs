e28567f9edd3894cac0594c92f21e8c563477166
rer <fodosoaodosoaoso@gmail.com>
2026-08-20 16:59:50 +0900

    図形のアウトラインノードの動的ポート化

--- YukkuriMovieMaker.Plugin.Community/Effect/Video/Node/Nodes/Generator/Outline/OutlineNodes.cs
+++ YukkuriMovieMaker.Plugin.Community/Effect/Video/Node/Nodes/Generator/Outline/OutlineNodes.cs
@@ -5,6 +5,7 @@ using YukkuriMovieMaker.Controls;
 using YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.Editor.Attributes;
 using YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.Graph;
 using YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.Graph.Attributes;
+using YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.Graph.Port;
 using YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.ValueTypes;
 
 namespace YukkuriMovieMaker.Plugin.Community.Effect.Video.Node.Nodes.Generator.Outline;
@@ -19,27 +20,15 @@ public enum ShapeKind
     [Display(Name = "直線")] Line
 }
 
-[Node(typeof(OutlineCategory), "図形アウトライン生成", "指定した種類の図形のアウトラインデータを生成します。")]
-public class ShapeOutlineNode : NodeLogic
+public class ShapeCommonInputs : InputsContainer
 {
-    private SKPath? _path;
-
-    [InputPort("種類", "生成する図形の種類")]
-    [EnumPortControl(Default = 0, IsEditable = false, Items = typeof(ShapeKind))]
-    [PortColorSetting(nameof(Colors.DarkOrange))]
-    public int Kind
-    {
-        get => GetInput<int>();
-        set => SetInput(value);
-    }
-
     [InputPort("中心X", "図形の中心のX座標（直線の場合は始点）")]
     [NumberPortControl(Min = -40000, Max = 40000, Default = 0)]
     [PortColorSetting(nameof(Colors.DarkOrange))]
     public float CenterX
     {
-        get => GetInput<float>();
-        set => SetInput(value);
+        get;
+        set => Set(ref field, value);
     }
 
     [InputPort("中心Y", "図形の中心のY座標（直線の場合は始点）")]
@@ -47,8 +36,8 @@ public class ShapeOutlineNode : NodeLogic
     [PortColorSetting(nameof(Colors.DarkOrange))]
     public float CenterY
     {
-        get => GetInput<float>();
-        set => SetInput(value);
+        get;
+        set => Set(ref field, value);
     }
 
     [InputPort("幅", "図形の幅（直線の場合は終点までのX方向距離）")]
@@ -56,8 +45,8 @@ public class ShapeOutlineNode : NodeLogic
     [PortColorSetting(nameof(Colors.DarkOrange))]
     public float Width
     {
-        get => GetInput<float>();
-        set => SetInput(value);
+        get;
+        set => Set(ref field, value);
     }
 
     [InputPort("高さ", "図形の高さ（直線の場合は終点までのY方向距離）")]
@@ -65,35 +54,8 @@ public class ShapeOutlineNode : NodeLogic
     [PortColorSetting(nameof(Colors.DarkOrange))]
     public float Height
     {
-        get => GetInput<float>();
-        set => SetInput(value);
-    }
-
-    [InputPort("角丸半径", "角丸矩形の角の半径")]
-    [NumberPortControl(Min = 0, Max = 4000, Default = 20)]
-    [PortColorSetting(nameof(Colors.DarkOrange))]
-    public float CornerRadius
-    {
-        get => GetInput<float>();
-        set => SetInput(value);
-    }
-
-    [InputPort("頂点数", "多角形・星形の頂点数")]
-    [NumberPortControl(Min = 3, Max = 64, Digits = 0, Default = 5)]
-    [PortColorSetting(nameof(Colors.DarkOrange))]
-    public float Sides
-    {
-        get => GetInput<float>();
-        set => SetInput(value);
-    }
-
-    [InputPort("内径比", "星形の内側の頂点半径（外側に対する割合）")]
-    [NumberPortControl(Min = 0, Max = 100, Default = 50, Unit = "%")]
-    [PortColorSetting(nameof(Colors.DarkOrange))]
-    public float InnerRadiusRatio
-    {
-        get => GetInput<float>();
-        set => SetInput(value);
+        get;
+        set => Set(ref field, value);
     }
 
     [InputPort("回転", "図形を中心（始点）周りに回転させる角度")]
@@ -101,8 +63,104 @@ public class ShapeOutlineNode : NodeLogic
     [PortColorSetting(nameof(Colors.DarkOrange))]
     public float Rotation
     {
-        get => GetInput<float>();
+        get;
+        set => Set(ref field, value);
+    }
+}
+
+public class RectInputs : ShapeCommonInputs
+{
+}
+
+public class RoundRectInputs : ShapeCommonInputs
+{
+    private float _cornerRadius;
+
+    [InputPort("角丸半径", "角丸矩形の角の半径")]
+    [NumberPortControl(Min = 0, Max = 4000, Default = 20)]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public float CornerRadius
+    {
+        get => _cornerRadius;
+        set => Set(ref _cornerRadius, value);
+    }
+}
+
+public class OvalInputs : ShapeCommonInputs
+{
+}
+
+public class PolygonInputs : ShapeCommonInputs
+{
+    private float _sides;
+
+    [InputPort("頂点数", "多角形の頂点数")]
+    [NumberPortControl(Min = 3, Max = 64, Digits = 0, Default = 5)]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public float Sides
+    {
+        get => _sides;
+        set => Set(ref _sides, value);
+    }
+}
+
+public class StarInputs : ShapeCommonInputs
+{
+    private float _innerRadiusRatio;
+    private float _sides;
+
+    [InputPort("頂点数", "星形の外側の頂点数")]
+    [NumberPortControl(Min = 3, Max = 64, Digits = 0, Default = 5)]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public float Sides
+    {
+        get => _sides;
+        set => Set(ref _sides, value);
+    }
+
+    [InputPort("内径比", "星形の内側の頂点半径（外側に対する割合）")]
+    [NumberPortControl(Min = 0, Max = 100, Default = 50, Unit = "%")]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public float InnerRadiusRatio
+    {
+        get => _innerRadiusRatio;
+        set => Set(ref _innerRadiusRatio, value);
+    }
+}
+
+public class LineInputs : ShapeCommonInputs
+{
+}
+
+[Node(typeof(OutlineCategory), "図形アウトライン生成", "指定した種類の図形のアウトラインデータを生成します。")]
+public class ShapeOutlineNode : NodeLogic
+{
+    private readonly LineInputs _lineInputs = new();
+    private readonly OvalInputs _ovalInputs = new();
+    private readonly PolygonInputs _polygonInputs = new();
+    private readonly RectInputs _rectInputs = new();
+    private readonly RoundRectInputs _roundRectInputs = new();
+    private readonly StarInputs _starInputs = new();
+
+    private ShapeKind _kind = ShapeKind.Rect;
+    private SKPath? _path;
+
+    [InputPort("種類", "生成する図形の種類")]
+    [EnumPortControl(Default = 0, IsEditable = false, Items = typeof(ShapeKind))]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public ShapeKind Kind
+    {
+        get => GetInput<ShapeKind>();
         set => SetInput(value);
+    }
+
+
+    [InputPort("パラメータ", "図形の種類に応じた入力群", isDynamic: true)]
+    [PortColorSetting(nameof(Colors.DarkOrange))]
+    public InputsContainer Params
+    {
+        get => GetCurrentInputs();
+        set => SetDynamicContainer(value);
     }
 
     [OutputPort("アウトライン", "生成された図形のアウトラインデータ")]
@@ -113,51 +171,137 @@ public class ShapeOutlineNode : NodeLogic
         set => SetOutput(value);
     }
 
+    protected internal override void OnInputValueChanged(string portName, object? value)
+    {
+        if (portName != nameof(Kind))
+            return;
+
+        var kind = value is ShapeKind shapeKind
+            ? shapeKind
+            : (ShapeKind)Convert.ToInt32(value);
+
+        if (kind == _kind)
+            return;
+
+        _kind = kind;
+
+        SetDynamicContainer(GetCurrentInputs(), nameof(Params));
+    }
+
+    private InputsContainer GetCurrentInputs()
+    {
+        return _kind switch
+        {
+            ShapeKind.Rect => _rectInputs,
+            ShapeKind.RoundRect => _roundRectInputs,
+            ShapeKind.Oval => _ovalInputs,
+            ShapeKind.Polygon => _polygonInputs,
+            ShapeKind.Star => _starInputs,
+            ShapeKind.Line => _lineInputs,
+            _ => _rectInputs
+        };
+    }
+
     protected override Task Calculate()
     {
+        var centerX = GetDynamicValue<float>("CenterX");
+        var centerY = GetDynamicValue<float>("CenterY");
+        var width = GetDynamicValue<float>("Width");
+        var height = GetDynamicValue<float>("Height");
+        var rotation = GetDynamicValue<float>("Rotation");
+
         var builder = new SKPathBuilder();
 
-        switch ((ShapeKind)Kind)
+        switch (_kind)
         {
             case ShapeKind.Rect:
-                builder.AddRect(new SKRect(CenterX - Width / 2f, CenterY - Height / 2f, CenterX + Width / 2f,
-                    CenterY + Height / 2f));
+            {
+                builder.AddRect(
+                    new SKRect(centerX - width / 2f, centerY - height / 2f, centerX + width / 2f,
+                        centerY + height / 2f));
+
                 break;
+            }
+
             case ShapeKind.RoundRect:
+            {
+                var cornerRadius = GetDynamicValue<float>("CornerRadius");
+
                 builder.AddRoundRect(
-                    new SKRect(CenterX - Width / 2f, CenterY - Height / 2f, CenterX + Width / 2f,
-                        CenterY + Height / 2f),
-                    System.Math.Max(0, CornerRadius), System.Math.Max(0, CornerRadius));
+                    new SKRect(centerX - width / 2f, centerY - height / 2f, centerX + width / 2f,
+                        centerY + height / 2f),
+                    System.Math.Max(0, cornerRadius), System.Math.Max(0, cornerRadius));
+
                 break;
+            }
+
             case ShapeKind.Oval:
-                builder.AddOval(new SKRect(CenterX - Width / 2f, CenterY - Height / 2f, CenterX + Width / 2f,
-                    CenterY + Height / 2f));
+            {
+                builder.AddOval(
+                    new SKRect(centerX - width / 2f, centerY - height / 2f, centerX + width / 2f,
+                        centerY + height / 2f));
+
                 break;
+            }
+
             case ShapeKind.Polygon:
-                builder.AddPoly(BuildStarPoints(CenterX, CenterY, Width / 2f, Height / 2f, Width / 2f, Height / 2f,
-                    System.Math.Max(3, (int)Sides)));
+            {
+                var sides = System.Math.Max(3, (int)GetDynamicValue<float>("Sides"));
+
+                builder.AddPoly(
+                    BuildStarPoints(centerX, centerY, width / 2f, height / 2f,
+                        width / 2f, height / 2f, sides));
+
                 break;
+            }
             case ShapeKind.Star:
-                builder.AddPoly(BuildStarPoints(CenterX, CenterY, Width / 2f, Height / 2f,
-                    Width / 2f * System.Math.Clamp(InnerRadiusRatio / 100f, 0f, 1f),
-                    Height / 2f * System.Math.Clamp(InnerRadiusRatio / 100f, 0f, 1f),
-                    System.Math.Max(3, (int)Sides) * 2));
+            {
+                var sides = System.Math.Max(3, (int)GetDynamicValue<float>("Sides"));
+                var innerRadiusRatio = System.Math.Clamp(
+                    GetDynamicValue<float>("InnerRadiusRatio") / 100f, 0f, 1f);
+
+                builder.AddPoly(
+                    BuildStarPoints(centerX, centerY, width / 2f, height / 2f,
+                        width / 2f * innerRadiusRatio, height / 2f * innerRadiusRatio, sides * 2));
+
                 break;
+            }
             case ShapeKind.Line:
-                builder.MoveTo(CenterX, CenterY);
-                builder.LineTo(CenterX + Width, CenterY + Height);
+            {
+                builder.MoveTo(centerX, centerY);
+                builder.LineTo(centerX + width, centerY + height);
+
                 break;
+            }
         }
 
         var path = builder.Detach();
-        if (Rotation != 0)
-            path.Transform(SKMatrix.CreateRotationDegrees(Rotation, CenterX, CenterY));
+
+        if (rotation != 0)
+            path.Transform(SKMatrix.CreateRotationDegrees(rotation, centerX, centerY));
 
         _path?.Dispose();
         _path = path;
         Output = new OutlineWrapper { Path = _path };
 
         return Task.CompletedTask;
+    }
+
+    private T GetDynamicValue<T>(string propertyName)
+    {
+        var portName = $"Params.{propertyName}";
+
+        if (!Inputs.TryGetValue(portName, out var port))
+            return default!;
+
+        var value = port
+            .GetValue(EvaluationContext)
+            .GetAwaiter()
+            .GetResult();
+
+        return value is T typed
+            ? typed
+            : default!;
     }
 
     private static SKPoint[] BuildStarPoints(float cx, float cy, float outerRx, float outerRy, float innerRx,
